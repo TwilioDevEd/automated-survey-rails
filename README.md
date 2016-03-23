59d5f8d08343f481720f8d388e592b230308cdc2
Agustin Camino <agustin.camino@gmail.com>
2016-03-23 17:04:59 -0500

    Here be dragons

---
+++ README.md
@@ -0,0 +1,5 @@
+# Automated Surveys with Ruby on Rails and Twilio
+
+This application demonstrates how to use Twilio and TwiML to perform automated phone surveys.
+
+[![Build Status](https://travis-ci.org/TwilioDevEd/automated-survey-rails.svg?branch=master)](https://travis-ci.org/TwilioDevEd/automated-survey-rails)
---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,25 @@
+class ConnectController < ApplicationController
+  def sms
+
+  end
+
+  def voice
+
+  end
+end
+
+class SurveysController < ApplicationController
+end
+
+class QuestionsController < ApplicationController
+end
+
+class AnswersController < ApplicationController
+end
+
+# /connect/sms
+# /connect/voice
+
+# /surveys
+# /surveys/1/questions/1
+# /surveys/1/questions/1/answers
