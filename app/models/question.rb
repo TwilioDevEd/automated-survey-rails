0946ab3fd7689a7bc55f01ece213bb1151d97c7a
Agustin Camino <agustin.camino@gmail.com>
2016-03-24 12:01:45 -0500

    Add question model

---
+++ app/models/question.rb
@@ -0,0 +1,9 @@
+class Question < ActiveRecord::Base
+  # Use type column without STI
+  self.inheritance_column = nil
+
+  enum type: { free: 0, numeric: 1, yes_no: 2 }
+
+  belongs_to :survey
+  has_many :answers, dependent: :destroy
+end
