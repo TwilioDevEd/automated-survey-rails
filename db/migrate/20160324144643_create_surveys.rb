fe40005fefd7e8d0c61cb3f45cb740af119ee304
Agustin Camino <agustin.camino@gmail.com>
2016-03-24 09:52:50 -0500

    Add migration to create surveys

---
+++ db/migrate/20160324144643_create_surveys.rb
@@ -0,0 +1,8 @@
+class CreateSurveys < ActiveRecord::Migration
+  def change
+    create_table :surveys do |t|
+      t.string :title, null: false
+      t.timestamps     null: false
+    end
+  end
+end
