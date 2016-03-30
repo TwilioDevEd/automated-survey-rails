bb0ef75c40a18e12c9c31c1bbba0842afac19c6d
Agustin Camino <agustin.camino@gmail.com>
2016-03-30 10:41:22 -0500

    Creates an answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,16 @@
 class AnswersController < ApplicationController
   def create
+    Answer.create(answer_params)
     render nothing: true
+  end
+
+  private
+
+  def answer_params
+    {
+      from:        params[:From],
+      content:     params[:RecordingUrl] || params[:Digits],
+      question_id: params[:question_id]
+    }
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,10 +1,45 @@
 require 'rails_helper'
 
 describe AnswersController do
+  let(:survey) { Survey.create(title: 'Bees') }
+  let(:question) do
+    Question.create(survey: survey, body: 'do you like bees?')
+  end
+
   describe '#create' do
+    subject { post :create, attributes_for_answer }
+
+    it 'creates an answer' do
+      expect do
+        subject
+      end.to change { Answer.count }.by(1)
+    end
+
+    it 'finds the next question'
+
+    context 'when there are available questions' do
+      it 'creates a response for the next question'
+    end
+
+    context 'when there are no more questions' do
+      it 'creates a response for closing the survey'
+    end
+
     it 'responds with ok' do
-      post :create
       expect(response).to be_ok
     end
+  end
+
+  private
+
+  # XXX: Check the response from Twilio
+  def attributes_for_answer
+    {
+      From:         '+14155559368',
+      Digits:       '1',
+      CallSid:      '12345',
+      question_id:  question.id,
+      RecordingUrl: 'http://example.com'
+    }
   end
 end
