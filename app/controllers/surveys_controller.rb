444d3a3caef6062dfebfa4473ff75c2411dcdf51
Agustin Camino <agustin.camino@gmail.com>
2016-03-31 16:57:07 -0500

    Respond with welcome message on SMS survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -11,6 +11,10 @@ class SurveysController < ApplicationController
 
   private
 
+  def user_response
+    params[:Body]
+  end
+
   def welcome_message_for_voice(survey)
     Twilio::TwiML::Response.new do |r|
       r.Say "Thank you for taking the #{survey.title} survey"
--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe SurveysController do
-  let(:survey)          { create(:survey, title: 'bees') }
-  let!(:first_question) { create(:question, survey: survey) }
+  let(:survey) { create(:survey, title: 'bees') }
 
   describe '#voice' do
+    let!(:question) { create(:question, survey: survey) }
+
     before { post :voice }
 
     it 'responds with a welcome message' do
@@ -16,7 +17,7 @@ describe SurveysController do
     it 'responds with a redirection to the first question' do
       document = Nokogiri::XML(response.body)
       expect(document.at_xpath('/Response/Redirect').content)
-        .to eq(question_path(first_question.id))
+        .to eq(question_path(question.id))
     end
 
     it 'responds with ok' do
@@ -25,16 +26,27 @@ describe SurveysController do
   end
 
   describe '#sms' do
-    before { post :sms }
+    let!(:first_question) { create(:question, survey: survey, body: 'first') }
+    let!(:last_question)  { create(:question, survey: survey, body: 'last') }
 
-    it 'responds with a welcome message' do
-      document = Nokogiri::XML(response.body)
-      expect(document.at_xpath('/Response/Message/Body').content)
-        .to eq('Thank you for taking the bees survey')
+    context 'when the first message arrives' do
+      before { post :sms }
+
+      it 'responds with a welcome message' do
+        expect(content_for('/Response/Message/Body'))
+          .to eq('Thank you for taking the bees survey')
+      end
     end
 
     it 'responds with ok' do
       expect(response).to be_ok
     end
+  end
+
+  private
+
+  def content_for(xpath)
+    document = Nokogiri::XML(response.body)
+    document.at_xpath(xpath).content
   end
 end
