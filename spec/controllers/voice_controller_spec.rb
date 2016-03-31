6ae1ee2ece453139f5d8007d8accb8c545cc7e4d
Agustin Camino <agustin.camino@gmail.com>
2016-03-31 00:25:12 -0500

    Test voice controller in integration

--- spec/controllers/voice_controller_spec.rb
+++ spec/controllers/voice_controller_spec.rb
@@ -2,24 +2,23 @@ require 'rails_helper'
 
 describe VoiceController do
   describe '#connect' do
+    let(:survey)    { create(:survey, title: 'bees') }
+    let!(:question) { create(:question, survey: survey) }
+
     before do
-      question = double(:question, id: 1)
-      survey = double(:survey, title: 'bees',
-                               first_question: question)
-      allow(Survey).to receive(:first) { survey }
       post :connect
     end
 
     it 'responds with `Say`' do
       document = Nokogiri::XML(response.body)
-      expect(document.at_xpath('//Response//Say').content)
+      expect(document.at_xpath('/Response/Say').content)
         .to eq('Thank you for taking the bees survey')
     end
 
     it 'responds with a `Redirect` to the first question' do
       document = Nokogiri::XML(response.body)
-      expect(document.at_xpath('//Response//Redirect').content)
-        .to eq(question_path(1))
+      expect(document.at_xpath('/Response/Redirect').content)
+        .to eq(question_path(question.id))
     end
 
     it 'responds with ok' do
