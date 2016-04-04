489c9da5f10134443c3077f14c6bcc5d088ac1f1
Agustin Camino <agustin.camino@gmail.com>
2016-04-03 19:55:14 -0500

    Allow Twilio to use the SMS webhook

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,4 +1,6 @@
 class SurveysController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def voice
     survey = Survey.first
     render xml: welcome_message_for_voice(survey)
